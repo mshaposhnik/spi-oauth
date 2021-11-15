240020849316c1fff669977e274836186f10e102
Max Shaposhnik <mshaposh@redhat.com>
2021-11-15 16:29:54 +0200

    fix fmt
    Signed-off-by: Max Shaposhnik <mshaposh@redhat.com>

--- main.go
+++ main.go
@@ -15,10 +15,8 @@ func main() {
 	router.HandleFunc("/github/authenticate", controllers.GitHubAuthenticate).Methods("GET")
 	router.HandleFunc("/github/callback", controllers.GitHubCallback).Methods("GET")
 
-
 	router.HandleFunc("/quay/authenticate", controllers.QuayAuthenticate).Methods("GET")
 	router.HandleFunc("/quay/callback", controllers.QuayCallback).Methods("GET")
-
 
 	port := os.Getenv("PORT")
 	if port == "" {
